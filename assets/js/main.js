4a03b9ed9a07dd4437e1256260fbf9a7fbd3f09d
alinemmonts <wyrd.dev@gmail.com>
2023-04-03 23:26:13 -0300

    portfolio update

---
+++ assets/js/main.js
@@ -0,0 +1,73 @@
+function updateProfileInfo(profileData) {
+    // const photo = document.getElementsById('profile.photo')
+    // photo.src = profileData.photo
+    // photo.alt = profileData.name
+
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+    
+    // const email = document.getElementById('profile.email')
+    // email.innerText = profileData.email
+    // email.href = `mailto:${profileData.email}`
+
+}
+
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills')
+
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="logo ${skill.name}" title="${skill.name}"></li>`).join('')
+}
+
+function updateLanguages(profileData) {
+    const languages = document.getElementById('profile.languages')
+
+    languages.innerHTML = profileData.languages.map(language => `<li><i class="fi fi-rs-check"></i>${language}</li>`).join('')
+}
+
+function updateProjects(profileData) {
+    const portfolio = document.getElementById('profile.portfolio')
+
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `
+             <li>
+                <span class="title">${project.name}</span> 
+                <a href="${project.url}" target="_blank"> 
+                ${project.github ? `<i class="fi fi-brands-github icon icon-project"><p>${project.url}</p></i>` : `<p>${project.url}</p>`} </a>
+            </li>`}).join('')
+    
+    
+}
+
+function updateProfessionalExperience(profileData) {
+    const professionalExperience = document.getElementById('profile.professionalExperience')
+
+    professionalExperience.innerHTML = profileData.professionalExperience.map(experience => {
+        return`
+        <li>
+            <h3 class="title">${experience.name}</h3>
+            <span class="period"><i class="fi fi-ss-calendar"></i>${experience.period}</span>
+            <p>${experience.description}</p>
+        </li>`}).join('')
+}
+
+(async () => {
+    const profileData = await fetchProfileData()
+    updateProfileInfo(profileData)
+    updateSoftSkills(profileData)
+    updateHardSkills(profileData)
+    updateLanguages(profileData)
+    updateProjects(profileData)
+    updateProfessionalExperience(profileData)
+})()
